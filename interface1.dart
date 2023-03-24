78971ac7eae30a5f391fa4cbf31894008a6046dc
Bissan <bsbsq73@gmail.com>
2023-03-25 01:30:16 +0300

    interfac1

---
+++ interface1.dart
@@ -0,0 +1,19 @@
+**void main() {
+Animal l=new Lion();
+l.type();
+l.displayType1();
+}
+abstract class Animal{
+void type(){
+print("Animal");
+}
+void displayType1();
+
+}
+class Lion implements Animal{
+void type(){
+print("Loin");
+}
+void displayType1(){
+print("Hello");
+}
