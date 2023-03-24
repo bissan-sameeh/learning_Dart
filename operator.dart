36838db2fb8d5efbd61832a8daf00616b7b59ebd
Bissan <bsbsq73@gmail.com>
2023-03-25 01:01:25 +0300

    learning dart

---
+++ operator.dart
@@ -0,0 +1,14 @@
+void main() {
+  var m = 25;
+  var n = 4;
+  print("m+n - ${m + n}");
+  print("++m - ${++m}");
+  print("m++ - ${m++}");
+  print("--m - ${--m}");
+  print("m-- - ${m--}");
+  print("m-n - ${m - n}");
+  print("m/n - ${m / n}");
+  print("m*n - ${m * n}");
+  print("m%n - ${m % n}");
+  print("m~/n - ${m ~/ n}");
+}
